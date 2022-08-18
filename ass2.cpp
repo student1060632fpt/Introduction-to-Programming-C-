8d9cc82beec59e3f02a3078821c85213a69b8f36
hoaminzy <48762609+student1060632fpt@users.noreply.github.com>
2022-08-18 11:32:52 +0700

    done nmdt

--- ass2.cpp
+++ ass2.cpp
@@ -12,15 +12,69 @@ using namespace std;
 int move(string moves)
 {
   /*TODO: add your code here*/
- 
+  int x = 0, y = 0, length = moves.size();
+  for (int i = 0; i < length; i = i + 2)
+  {
+    char letter = moves[i];
+    int step = (int)moves[i + 1] - 48;
+    switch (letter)
+    {
+    case 'U':
+      x += step;
+      break;
+    case 'D':
+      x -= step;
+      break;
+    case 'L':
+      y -= step;
+      break;
+    case 'R':
+      y += step;
+      break;
+    default:
+      break;
+    }
+  }
+  float distanceNeedRough = sqrt(x * x + y * y);
+  return round(distanceNeedRough);
 }
 bool isPrime(int n)
 {
   /*TODO: add your code here*/
+  bool flag = true;
+
+  if (n < 2)
+    return flag = false;
+
+  int i = 2;
+  while (i < n)
+  {
+    if (n % i == 0)
+    {
+      flag = false;
+      break; /*Chỉ cần tìm thấy 1 ước số là đủ và thoát vòng lặp*/
+    }
+    i++;
+  }
+  return flag;
 }
 bool isSquared(int n)
 {
   /*TODO: add your code here*/
+  bool flag = false;
+
+  int i = 0;
+  while (i <= n)
+  {
+    if (pow(i, 2) == n)
+    {
+      flag = true;
+      break;
+    }
+    i++;
+  }
+
+  return flag;
 }
 int main()
 {
@@ -39,6 +93,7 @@ int main()
    * Remember to assign calculated value to variable `distance`
    */
   /*TODO: add your code here*/
+  distance = move(moves);
 
   /*---------------------------------*/
   cout << "Khoang cach xe da di chuyen tu vi tri ban dau toi diem hien tai: "
@@ -49,14 +104,14 @@ int main()
    * Remember to assign appropriate value to variable `primality`
    */
   /*TODO: add your code here*/
-
+  primality = isPrime(distance);
   /*---------------------------------*/
   /*---------------------------------
    * Check if distance is squared
    * Remember to assign appropriate value to variable `squared`
    */
   /*TODO: add your code here*/
-
+  squared = isSquared(distance);
   /*---------------------------------*/
   if (primality)
   {
